4d35a02ae5bc28cba4432d1f6ad3dab16c9e832d
Alec G <gallowayalec@gmail.com>
2024-07-09 15:35:08 -0600

    added some interface options to hide certain elements on the root

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,19 +1,40 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
-const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const title = fileData.frontmatter?.title
-  if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
-  } else {
-    return null
+interface ArticleTitleOptions {
+  /**
+   * Whether to display breadcrumbs on root `index.md`
+   */
+  hideOnRoot: boolean
+}
+
+const defaultOptions: ArticleTitleOptions = {
+  hideOnRoot: true,
+}
+
+export default ((opts?: Partial<ArticleTitleOptions>) => {
+  // Merge options with defaults
+  const options: ArticleTitleOptions = { ...defaultOptions, ...opts }
+
+  const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
+    // Hide title on root if enabled
+    if (options.hideOnRoot && fileData.slug === "index") {
+      return null
+    }
+
+    const title = fileData.frontmatter?.title
+    if (title) {
+      return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+    } else {
+      return null
+    }
   }
-}
 
-ArticleTitle.css = `
-.article-title {
-  margin: 2rem 0 0 0;
-}
-`
+  ArticleTitle.css = `
+  .article-title {
+    margin: 2rem 0 0 0;
+  }
+  `
 
-export default (() => ArticleTitle) satisfies QuartzComponentConstructor
+  return ArticleTitle
+}) satisfies QuartzComponentConstructor
--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -4,33 +4,54 @@ import { resolveRelative, simplifySlug } from "../util/path"
 import { i18n } from "../i18n"
 import { classNames } from "../util/lang"
 
-const Backlinks: QuartzComponent = ({
-  fileData,
-  allFiles,
-  displayClass,
-  cfg,
-}: QuartzComponentProps) => {
-  const slug = simplifySlug(fileData.slug!)
-  const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
-  return (
-    <div class={classNames(displayClass, "backlinks")}>
-      <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
-      <ul class="overflow">
-        {backlinkFiles.length > 0 ? (
-          backlinkFiles.map((f) => (
-            <li>
-              <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
-                {f.frontmatter?.title}
-              </a>
-            </li>
-          ))
-        ) : (
-          <li>{i18n(cfg.locale).components.backlinks.noBacklinksFound}</li>
-        )}
-      </ul>
-    </div>
-  )
+interface BacklinksOptions {
+  /**
+   * Whether to display breadcrumbs on root `index.md`
+   */
+  hideOnRoot: boolean
 }
 
-Backlinks.css = style
-export default (() => Backlinks) satisfies QuartzComponentConstructor
+const defaultOptions: BacklinksOptions = {
+  hideOnRoot: true,
+}
+
+export default ((opts?: Partial<BacklinksOptions>) => {
+  // Merge options with defaults
+  const options: BacklinksOptions = { ...defaultOptions, ...opts }
+
+  const Backlinks: QuartzComponent = ({
+    fileData,
+    allFiles,
+    displayClass,
+    cfg,
+  }: QuartzComponentProps) => {
+    // Hide backlinks on root if enabled
+    if (options.hideOnRoot && fileData.slug === "index") {
+      return null
+    }
+
+    const slug = simplifySlug(fileData.slug!)
+    const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
+    return (
+      <div class={classNames(displayClass, "backlinks")}>
+        <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
+        <ul class="overflow">
+          {backlinkFiles.length > 0 ? (
+            backlinkFiles.map((f) => (
+              <li>
+                <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
+                  {f.frontmatter?.title}
+                </a>
+              </li>
+            ))
+          ) : (
+            <li>{i18n(cfg.locale).components.backlinks.noBacklinksFound}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+
+  Backlinks.css = style
+  return Backlinks
+}) satisfies QuartzComponentConstructor
