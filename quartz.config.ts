2e562bbf25dd8f4f06f57691cea1df7f212aeb1a
Alec G <gallowayalec@gmail.com>
2024-07-11 11:19:37 -0600

    Quartz sync: Jul 11, 2024, 11:19 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "logs.ajgalloway.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
